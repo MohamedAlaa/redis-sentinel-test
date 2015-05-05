7895f68ca4e8d61382a4562efca2c60a6abb6f26
Dara Kong <darakong@gmail.com>
2015-05-05 12:11:04 -0700

    script

---
+++ kill.sh
@@ -0,0 +1,1 @@
+pgrep redis | xargs kill
---
+++ start.sh
@@ -0,0 +1,6 @@
+redis-server redis6001.conf &
+redis-server redis7001.conf &
+
+redis-sentinel sentinel1.conf &
+redis-sentinel sentinel2.conf &
+redis-sentinel sentinel3.conf &
