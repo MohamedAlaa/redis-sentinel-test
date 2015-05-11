b8aefb00dd31f7f64663fa43994cd81815ab7ce8
Dara Kong <darakong@gmail.com>
2015-05-11 22:48:58 +0000

    latest

---
+++ write-default-sentinel.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+
+count = int(sys.argv[1])
+quorum = int(sys.argv[2])
+
+template = """sentinel monitor %(name)s 127.0.0.1 %(port)s %(quorum)s
+sentinel down-after-milliseconds %(name)s 3000
+sentinel parallel-syncs %(name)s 1
+sentinel failover-timeout %(name)s 60000
+"""
+
+with open('default-sentinel.conf', 'wt') as f:
+    for i in range(1, count + 1):
+        port = 6000 + i
+        f.write(template % {
+                'port':port,
+                'quorum':quorum,
+                'name':'master%s' % port
+            })
