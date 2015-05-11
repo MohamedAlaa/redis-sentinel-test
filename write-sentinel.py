fe884bc5569daf9b8452c0421860bc304b81d8ca
Dara Kong <darakong@gmail.com>
2015-05-11 12:11:34 -0700

    latest

--- write-sentinel.py
+++ write-sentinel.py
@@ -1,8 +1,9 @@
 import sys
 
-template = """include sentinel-default.conf
+template = """include default-sentinel.conf
 port %s"""
 
 for i in range(1, int(sys.argv[1]) + 1):
-    with open('sentinel%d.conf' % i) as f:
-        f.write(template % i)
+    port = 8000 + i
+    with open('sentinel%d.conf' % port, 'wt') as f:
+        f.write(template % port)
